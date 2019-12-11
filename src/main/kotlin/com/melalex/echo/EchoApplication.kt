33ae9ac0103e779302ab8e89778b1514770a8e33
Alexander Melashchenko <melalex490@gmail.com>
2019-12-11 21:48:30 +0200

    Initial commit

---
+++ src/main/kotlin/com/melalex/echo/EchoApplication.kt
@@ -0,0 +1,11 @@
+package com.melalex.echo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EchoApplication
+
+fun main(args: Array<String>) {
+    runApplication<EchoApplication>(*args)
+}
